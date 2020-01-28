230a4e31a0ec96082b88447dae1c8f45580c1563
Vivien Kuo <vivien.kuo@sproutsocial.com>
2020-01-28 15:43:28 -0600

    skeleton with heroku server

--- src/main/java/CompleteableFutureService.java
+++ src/main/java/CompleteableFutureService.java
@@ -1,18 +1,11 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Instant;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CompleteableFutureService {
-    public static final String ASYNC_SERVICE_ENDPOINT = "http://127.0.0.1:3000";
+    public static final String ASYNC_SERVICE_ENDPOINT = "https://node-async-server.herokuapp.com/";
     public static final String WAIT_AND_SUCCESS = ASYNC_SERVICE_ENDPOINT + "/waitAndRespond";
     public static final String WAIT_AND_ERROR = ASYNC_SERVICE_ENDPOINT + "/waitAndError";
     public static final String ASYNC_PUBLISH_ENDPOINT = ASYNC_SERVICE_ENDPOINT + "/publish";
@@ -26,85 +19,29 @@ public class CompleteableFutureService {
     // https://openjdk.java.net/groups/net/httpclient/recipes.html
     // https://www.baeldung.com/java-completablefuture
     // https://4comprehension.com/completablefuture-the-difference-between-thenapply-thenapplyasync/
-    public static void main(String[] args) throws ExecutionException, InterruptedException, JsonProcessingException {
-        hitPublishEndpoint();
+    public static void main(String[] args) {
+    }
+
+    private static HttpRequest buildRequest(String url) {
+        return HttpRequest.newBuilder().build();
     }
 
     public static void asyncRequestExample() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(WAIT_AND_SUCCESS))
-                .POST(HttpRequest.BodyPublishers.ofString(MOCK_DATA))
-                .build();
-
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
-
+        // write an async request with the native Java client
     }
 
-    public static void twoAsyncRequestsExample() throws ExecutionException, InterruptedException {
-        HttpRequest request1 = HttpRequest.newBuilder()
-                .uri(URI.create(WAIT_AND_SUCCESS + "?seconds=4"))
-                .POST(HttpRequest.BodyPublishers.ofString(MOCK_DATA))
-                .build();
-
-        HttpRequest request2 = HttpRequest.newBuilder()
-                .uri(URI.create(WAIT_AND_SUCCESS + "?seconds=6"))
-                .POST(HttpRequest.BodyPublishers.ofString(MOCK_DATA))
-                .build();
-
-        CompletableFuture<String> future1 = client.sendAsync(request1, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body);
-        CompletableFuture<String> future2 = client.sendAsync(request2, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body);
-
-        long start = Instant.now().toEpochMilli();
-
-        String combined = Stream.of(future1, future2)
-                .map(CompletableFuture::join)
-                .collect(Collectors.joining(", "));
-
-        System.out.println(Instant.now().toEpochMilli() - start);
-        System.out.println(combined);
+    public static void twoTimedAsyncRequestsExample() {
+        // make 2 separate requests and time both requests to prove that they are running parallel
     }
 
     public static void asyncRequestAndFail() {
-        HttpRequest requestFailure = HttpRequest.newBuilder()
-                .uri(URI.create(WAIT_AND_ERROR + "?seconds=4"))
-                .POST(HttpRequest.BodyPublishers.ofString(MOCK_DATA))
-                .build();
-
-        client.sendAsync(requestFailure, HttpResponse.BodyHandlers.ofString())
-                .thenApplyAsync((resp) -> {
-                    if (resp.statusCode() >= 400) {
-                        System.err.println("Error!! " + resp.statusCode());
-                    }
-                    return resp.body();
-                }).join();
+        // make a request that anticipates a 400
+        // What is the best way to handle an endpoint that could return 400s?
     }
 
-    public static void hitPublishEndpoint() throws JsonProcessingException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(ASYNC_PUBLISH_ENDPOINT))
-                .POST(HttpRequest.BodyPublishers.ofString(MOCK_DATA))
-                .build();
-
-        String response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .join();
-
-        PublishResponse publishResponse = objectMapper.readValue(response, PublishResponse.class);
-        HttpRequest request2 = HttpRequest.newBuilder()
-                .uri(URI.create(STATUS_ENDPOINT + "?jobId=" + publishResponse.getJobId()))
-                .build();
-
-        String response2 = client.sendAsync(request2, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .join();
-
-        StatusResponse statusResponse = objectMapper.readValue(response2, StatusResponse.class);
-
-        System.out.print(statusResponse.getStatus());
+    public static void hitPublishEndpoint() {
+        // Hit the ASYNC_SERVICE_ENDPOINT, and using the jobId from PublishResponse
+        // poll ASYNC_SERVICE_ENDPOINT to check to see when the job completes
+        // exit the program with "Hooray!" when StatusResponse.getStatus == "complete"
     }
 }
